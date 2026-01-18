8a2cb45d52f740091adecc983c04f5fe687da85b
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-01-18 18:32:54 -0500

    uic-zig: Update enum support

--- cmd/uic-zig/ui2zig.go
+++ cmd/uic-zig/ui2zig.go
@@ -440,13 +440,6 @@ func renderProperties(properties []UiProperty, ret *strings.Builder, targetName,
 		} else if prop.SetVal != nil {
 			// QDialogButtonBox::StandardButton::*
 			// <set>QDialogButtonBox::StandardButton::Cancel|QDialogButtonBox::StandardButton::Save</set>
-
-			// block KRichTextWidget::setRichTextSupport due to poor implementation
-			if prop.Name == "richTextSupport" {
-				ret.WriteString("// UIC: Property `" + prop.Name + "` is not supported for type " + targetClass + "\n")
-				continue
-			}
-
 			parts := strings.Split(*prop.SetVal, "|")
 			for i, p := range parts {
 				parts[i] = normalizeEnumName(prop.Name, p)
@@ -456,7 +449,13 @@ func renderProperties(properties []UiProperty, ret *strings.Builder, targetName,
 			if len(parts) > 0 {
 				emit = strings.Join(parts, "|")
 			}
-			ret.WriteString(zigQtStruct + setterFunc + "(ui." + targetName + ", " + emit + ");\n")
+
+			if prop.Name == "richTextSupport" {
+				ret.WriteString("const " + targetName + "_" + prop.Name + " = " + emit + ";\n")
+				ret.WriteString(zigQtStruct + setterFunc + "(ui." + targetName + ", &" + targetName + "_" + prop.Name + ");\n")
+			} else {
+				ret.WriteString(zigQtStruct + setterFunc + "(ui." + targetName + ", " + emit + ");\n")
+			}
 
 		} else if prop.IconVal != nil {
 			iconName := renderIcon(prop.IconVal, ret)
