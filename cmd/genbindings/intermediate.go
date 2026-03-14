28caf7d6b6dfa11da3020c8725dcd4a87631eb4b
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-03-14 13:55:17 -0400

    intermediate.go: Update type mapping

--- cmd/genbindings/intermediate.go
+++ cmd/genbindings/intermediate.go
@@ -136,6 +136,11 @@ func (p CppParameter) QtClassType() bool {
 		return true
 	}
 
+	// as of Qt 6.8, quint128 is detected as a class
+	if p.ParameterType == "quint128" {
+		return false
+	}
+
 	// Maybe if it's an inner class
 	if _, ok := KnownClassnames[p.ParameterType]; ok {
 		return true
@@ -154,6 +159,11 @@ func (p CppParameter) IsKnownEnum() bool {
 }
 
 func IsKnownClass(className string) bool {
+	// as of Qt 6.8, quint128 is detected as a class
+	if className == "quint128" {
+		return false
+	}
+
 	_, ok := KnownClassnames[className]
 	return ok
 }
@@ -288,7 +298,7 @@ func (p CppParameter) IntType() bool {
 		"QIntegerForSizeof<void *>::Signed",
 		"QIntegerForSizeof<std::size_t>::Signed",
 		"qptrdiff", "ptrdiff_t",
-		"double", "float", "qreal":
+		"double", "float", "qreal", "quint128":
 		return true
 
 	case "char":
