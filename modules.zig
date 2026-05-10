411b9aa7ed1c0bf78672eafa7b5147965dfefe6b
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-05-10 15:39:34 -0400

    modules.zig: Add KTextAddons and separate module data

---
+++ modules.zig
@@ -0,0 +1,209 @@
+pub const modules = &.{
+    // Qt 6 Core, GUI, Widgets
+    "QtCore",
+    "QtGui",
+    "QtWidgets",
+    // Qt 6 Bluetooth
+    "QtBluetooth",
+    // Qt 6 Charts
+    "QtCharts",
+    // Qt 6 D-Bus
+    "QtDBus",
+    // Qt 6 Location
+    "QtLocation",
+    // Qt 6 Multimedia
+    "QtMultimedia",
+    "QtMultimediaWidgets",
+    // Qt 6 Network
+    "QtNetwork",
+    // Qt 6 OpenGL
+    "QtOpenGL",
+    "QtOpenGLWidgets",
+    // Qt 6 PDF
+    "QtPdf",
+    "QtPdfWidgets",
+    // Qt 6 Positioning
+    "QtPositioning",
+    // Qt 6 Print Support
+    "QtPrintSupport",
+    // Qt 6 Spatial Audio
+    "QtSpatialAudio",
+    // Qt 6 SQL
+    "QtSql",
+    // Qt 6 State Machine
+    "QtStateMachine",
+    // Qt 6 SVG
+    "QtSvg",
+    "QtSvgWidgets",
+    // Qt 6 TextToSpeech
+    "QtTextToSpeech",
+    // Qt 6 UI Plugin
+    "QtUiPlugin",
+    // Qt 6 UI Tools
+    "QtUiTools",
+    // Qt 6 Designer
+    "QtDesigner",
+    // Qt 6 WebChannel
+    "QtWebChannel",
+    "QtWebChannelQuick",
+    // Qt 6 WebEngine
+    "QtWebEngineCore",
+    "QtWebEngineWidgets",
+    // Qt 6 WebSockets
+    "QtWebSockets",
+    // Qt 6 XML
+    "QtXml",
+    // Qt 6 KArchive
+    "KArchive",
+    // Qt 6 Attica
+    "Attica",
+    "Attica/Attica",
+    "Attica/attica",
+    // Qt 6 KCodecs
+    "KCodecs",
+    // Qt 6 kColorPicker
+    "kColorPicker-Qt6",
+    "kColorPicker-Qt6/kColorPicker",
+    // Qt 6 KCompletion
+    "KCompletion",
+    // Qt 6 KConfig
+    "KConfig",
+    "KConfigCore",
+    "KConfigGui",
+    // Qt 6 KCoreAddons
+    "KCoreAddons",
+    // Qt 6 KCrash
+    "KCrash",
+    // Qt 6 KFileMetaData
+    "KFileMetaData",
+    "KFileMetaData/kfilemetadata",
+    // Qt 6 KGuiAddons
+    "KGuiAddons",
+    // Qt 6 KI18n
+    "KI18n",
+    "KI18nLocaleData",
+    // Qt 6 KIdleTime
+    "KIdleTime",
+    // Qt 6 kImageAnnotator
+    "kImageAnnotator-Qt6",
+    "kImageAnnotator-Qt6/kImageAnnotator",
+    // Qt 6 KItemModels
+    "KItemModels",
+    // Qt 6 KItemViews
+    "KItemViews",
+    // Qt 6 KJobWidgets
+    "KJobWidgets",
+    // Qt 6 KNewStuff
+    "KNewStuff",
+    "KNewStuffCore",
+    "KNewStuffCore/KNSCore",
+    "KNewStuffWidgets",
+    "KNewStuffWidgets/KNSWidgets",
+    // Qt 6 KPlotting
+    "KPlotting",
+    // Qt 6 KService
+    "KService",
+    // Qt 6 Solid
+    "Solid",
+    "Solid/Solid",
+    "Solid/solid",
+    // Qt 6 Sonnet
+    "Sonnet",
+    "SonnetCore",
+    "SonnetCore/sonnet",
+    "SonnetUi",
+    "SonnetUi/sonnet",
+    // Qt 6 KStatusNotifierItem
+    "KStatusNotifierItem",
+    // Qt 6 KSvg
+    "KSvg",
+    "KSvg/KSvg",
+    "KSvg/ksvg",
+    // Qt 6 KSyntaxHighlighting
+    "KSyntaxHighlighting",
+    "KSyntaxHighlighting/KSyntaxHighlighting",
+    // Qt 6 KTextAddons
+    "TextAddonsWidgets/textaddonswidgets",
+    "TextAutoCorrectionCore",
+    "TextAutoCorrectionCore/textautocorrectioncore",
+    "TextAutoCorrectionWidgets/textautocorrectionwidgets",
+    "TextCustomEditor",
+    "TextCustomEditor/textcustomeditor",
+    "TextEditTextToSpeech",
+    "TextEditTextToSpeech/textedittexttospeech",
+    "TextEmoticonsCore",
+    "TextEmoticonsCore/textemoticonscore",
+    "TextEmoticonsWidgets",
+    "TextEmoticonsWidgets/textemoticonswidgets",
+    "TextGrammarCheck",
+    "TextGrammarCheck/textgrammarcheck",
+    "TextTranslator",
+    "TextTranslator/texttranslator",
+    "TextUtils/textutils",
+    // Qt 6 KTextWidgets
+    "KTextWidgets",
+    // Qt 6 KUnitConversion
+    "KUnitConversion",
+    "KUnitConversion/KUnitConversion",
+    "KUnitConversion/kunitconversion",
+    // Qt 6 KWidgetsAddons
+    "KWidgetsAddons",
+    // Qt 6 KColorScheme
+    "KColorScheme",
+    // Qt 6 KConfigWidgets
+    "KConfigWidgets",
+    // Qt 6 KBookmarks
+    "KBookmarks",
+    "KBookmarksWidgets",
+    // Qt 6 KNotifications
+    "KNotifications",
+    // Qt 6 KIconThemes
+    "KIconThemes",
+    "KIconWidgets",
+    // Qt 6 KXmlGui
+    "KXmlGui",
+    // Qt 6 QtKeychain
+    "qt6keychain",
+    // Qt 6 LayerShellQt
+    "LayerShellQt",
+    // Qt 6 KGlobalAccel
+    "KGlobalAccel",
+    // Qt 6 KWindowSystem
+    "KWindowSystem",
+    // Qt 6 PackageKit-Qt
+    "PackageKitQt",
+    "PackageKitQt/PackageKit",
+    "packagekitqt6",
+    "packagekitqt6/PackageKit",
+    // Qt 6 Accounts
+    "accounts-qt6",
+    "accounts-qt6/Accounts",
+    // Qt 6 SignOn
+    "signon-qt6",
+    "signon-qt6/SignOn",
+    // Qt 6 KIO
+    "KIO",
+    "KIOCore",
+    "KIOCore/kio",
+    "KIOFileWidgets",
+    "KIOGui",
+    "KIOGui/kio",
+    "KIOWidgets",
+    "KIOWidgets/kio",
+    // Qt 6 KParts
+    "KParts",
+    "KParts/KParts",
+    "KParts/kparts",
+    // Qt 6 KTextEditor
+    "KTextEditor",
+    "KTextEditor/KTextEditor",
+    "KTextEditor/ktexteditor",
+    // Qt 6 Poppler
+    "poppler/qt6",
+    // Qt 6 QCustomPlot
+    "QCustomPlotQt6",
+    // Qt 6 QScintilla
+    "Qsci",
+    // Qt 6 QTermWidget
+    "qtermwidget6",
+};
